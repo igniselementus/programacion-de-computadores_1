ebfddae5f35c4806629490c49ea7089cd21ae4c5
diego herrera <d.herrera07@ufromail.cl>
2021-10-25 17:13:01 -0300

    lab_8

--- lab_8/actividad_1.c
+++ lab_8/actividad_1.c
@@ -1,8 +1,81 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+char tablero[3][3];
+int fila;
+int columna;
+int ganador = 0;
+int turnos = 0;
+
+/* 0=falso, 1=verdadero*/
 
 
 int main(){
-  
 
-return 0;
-}
+    for(int i = 0 ; i < 3 ; i++){
+        for(int j = 0 ; j < 3 ; j++){
+            tablero[i][j] = '-';
+        }
+    }
+
+    printf("%c  |  %c  |  %c\n", tablero[0][0], tablero[0][1], tablero[0][2]);
+    printf("----------------\n");
+    printf("%c  |  %c  |  %c\n", tablero[1][0], tablero[1][1], tablero[1][2]);
+    printf("----------------\n");
+    printf("%c  |  %c  |  %c\n", tablero[2][0], tablero[2][1], tablero[2][2]);
+
+    while(!ganador && turnos < 9){
+        
+        if(turnos%2 == 0){
+            printf("Jugador X -> Ingrese su jugada de la forma fila,columna:\n");
+            scanf("%d,%d", &fila, &columna);
+
+            // PEDIRLE LA JUGADA HASTA QUE ELIJA UNA CASILLA QUE ESTA DISPONIBLE
+            // MIENTRAS LA CASILLA ESTE OCUPADA, TENGO QUE PEDIR NUEVAMENTE LA JUGADA
+
+            tablero[fila - 1][columna - 1] = 'X'; 
+        } else {
+            printf("Jugador O -> Ingrese su jugada de la forma fila,columna:\n");
+            scanf("%d,%d", &fila, &columna);
+            tablero[fila - 1][columna - 1] = 'O'; 
+        }
+
+        printf("%c  |  %c  |  %c\n", tablero[0][0], tablero[0][1], tablero[0][2]);
+        printf("----------------\n");
+        printf("%c  |  %c  |  %c\n", tablero[1][0], tablero[1][1], tablero[1][2]);
+        printf("----------------\n");
+        printf("%c  |  %c  |  %c\n", tablero[2][0], tablero[2][1], tablero[2][2]);
+
+        // FILAS
+        if((tablero[0][0] == tablero[0][1]) && (tablero[0][1] == tablero[0][2]) && (tablero[0][0] != '-')){
+            printf("GANADOR!!");
+            ganador = 1;
+        }
+        if((tablero[1][0] == tablero[1][1]) && (tablero[1][1] == tablero[1][2]) && (tablero[1][0] != '-')){
+            printf("GANADOR!!");
+            ganador = 1;
+        }
+        if((tablero[2][0] == tablero[2][1]) && (tablero[2][1] == tablero[2][2]) && (tablero[2][0] != '-')){
+            printf("GANADOR!!");
+            ganador = 1;
+        }
+
+        // COLUMNAS
+        if((tablero[0][0] == tablero[1][0]) && (tablero[1][0] == tablero[2][0]) && (tablero[0][0] != '-')){
+            printf("GANADOR!!");
+            ganador = 1;
+        }
+
+        // COMPLETAR LAS DOS COLUMNAS QUE FALTAN
+        // EVALUAR LAS DOS DIAGONALES
+        // EVITAR QUE UNA JUGADA SE SOBREESCRIBA
+
+        turnos++;
+    }
+
+
+
+
+    return 0; 
+} 
